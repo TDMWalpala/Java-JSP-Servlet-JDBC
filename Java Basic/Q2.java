cc521cf09734bd75dc66c5422af5567ebd8b0575
TDMWalpala <tharindudarshana2019@gmail.com>
2021-08-14 00:31:16 +0530

    Added files and dirs into the repo

---
+++ Java Basic/Q2.java
@@ -0,0 +1,12 @@
+public class Q2{
+	public static void main(String args[]){
+		int a=3,b=4,c=5,d=6,e=7,Total;
+		float Avg;
+		Total=(a+b+c+d+e);
+		Avg= Total/5;
+		System.out.print("Total :");
+		System.out.println(Total);
+		System.out.print("Average :");
+		System.out.println(Avg);
+	}
+}
---
+++ Java Basic/Q3.java
@@ -0,0 +1,12 @@
+public class Q3{
+	public static void main(String args[]){
+		System.out.println("----------------------------------------");
+		System.out.print("|\tFrist Name Tharindu");
+		System.out.print("        \t|\n");
+		System.out.print("|\tLast Name  Darshana");
+		System.out.print("        \t|\n");
+		System.out.print("|\tFull Name  Tharindu Darshana");
+		System.out.print("\t|\n");
+		System.out.println("-----------------------------------------");
+	}
+}
