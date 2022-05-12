0145037840f000e7d1a5b12f0e8552b080974400
TDMWalpala <tharindudarshana2019@gmail.com>
2022-05-12 18:34:05 +0530

    Servlet Annotation Configuration

--- DemoApp/src/main/java/com/addServlet/SquareServlet.java
+++ DemoApp/src/main/java/com/addServlet/SquareServlet.java
@@ -7,9 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 
-
+@WebServlet("/sqr")
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
 		 int sum = 0;
